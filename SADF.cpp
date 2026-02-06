510b125b49d0f557d0eab4cee300186735230bd5
Areo Mche <areo.mche.ce@gmail.com>
2026-02-06 16:03:59 +0330

    adding and connecting 2048 with basic

--- SADF.cpp
+++ SADF.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <windows.h>
 #include <conio.h>
+#include <ctime>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <string>
+
 
 using namespace std;
 
@@ -82,7 +88,7 @@ void startSection()
         SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), r);
         cout << "DF";
 
-        Sleep(120);
+        Sleep(90);
     }
 
     clear();
@@ -211,6 +217,249 @@ int game7667Menu()
         }
     }
 }
+
+
+int score = 0;
+string playerName;
+void setColor2048(int color)
+{
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+
+void printjadval(int jadval[4][4])
+{
+    cout << "#------#------#------#------#\n";
+    for (int i = 0; i < 4; i++)
+    {
+        cout << "|";
+        for (int j = 0; j < 4; j++)
+        {
+            if (jadval[i][j] == 0)
+            {
+                cout << "      |";
+            }
+            else
+            {
+                int val = jadval[i][j];
+
+                if (val == 2)
+                    setColor2048(9);
+                else if (val == 4)
+                    setColor2048(12);
+                else if (val == 8)
+                    setColor2048(10);
+                else if (val == 16)
+                    setColor2048(14);
+                else if (val == 32)
+                    setColor2048(11);
+                else if (val == 64)
+                    setColor2048(13);
+                else
+                    setColor2048(15);
+
+                printf(" %4d ", val);
+                setColor2048(7);
+                cout << "|";
+            }
+        }
+        cout << "\n#------#------#------#------#\n";
+    }
+}
+void moveleft(int a[4])
+{
+    int chap[4] = {0, 0, 0, 0};
+    int index = 0;
+    for (int i = 0; i < 4; i++)
+        if (a[i] != 0)
+            chap[index++] = a[i];
+
+    for (int i = 0; i < 3; i++)
+    {
+        if (chap[i] != 0 && chap[i] == chap[i + 1])
+        {
+            chap[i] *= 2;
+            score += chap[i];
+            chap[i + 1] = 0;
+        }
+    }
+
+    int b[4] = {0, 0, 0, 0};
+    int index2 = 0;
+    for (int i = 0; i < 4; i++)
+        if (chap[i] != 0)
+            b[index2++] = chap[i];
+
+    for (int i = 0; i < 4; i++)
+        a[i] = b[i];
+}
+
+void moveright(int a[4])
+{
+    int reversed[4];
+    for (int i = 0; i < 4; i++)
+        reversed[i] = a[3 - i];
+    moveleft(reversed);
+    for (int i = 0; i < 4; i++)
+        a[i] = reversed[3 - i];
+}
+
+void transpose(int jadval[4][4])
+{
+    for (int i = 0; i < 4; i++)
+        for (int j = i + 1; j < 4; j++)
+            swap(jadval[i][j], jadval[j][i]);
+}
+
+void moverightjadval(int jadval[4][4])
+{
+    for (int i = 0; i < 4; i++)
+        moveright(jadval[i]);
+}
+
+void moveleftjadval(int jadval[4][4])
+{
+    for (int i = 0; i < 4; i++)
+        moveleft(jadval[i]);
+}
+
+void moveupjadval(int jadval[4][4])
+{
+    transpose(jadval);
+    moveleftjadval(jadval);
+    transpose(jadval);
+}
+
+void movedownjadval(int jadval[4][4])
+{
+    transpose(jadval);
+    moverightjadval(jadval);
+    transpose(jadval);
+}
+
+void addrandomnumber(int jadval[4][4])
+{
+    int count = 0;
+    for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 4; j++)
+            if (jadval[i][j] == 0)
+                count++;
+
+    if (count == 0)
+        return;
+
+    int r = rand() % count;
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            if (jadval[i][j] == 0)
+            {
+                if (r == 0)
+                {
+                    jadval[i][j] = (rand() % 10 < 9) ? 2 : 4;
+                    return;
+                }
+                r--;
+            }
+        }
+    }
+}
+bool checklose(int jadval[4][4])
+{
+    for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 4; j++)
+            if (jadval[i][j] == 0)
+                return false;
+
+    for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 3; j++)
+            if (jadval[i][j] == jadval[i][j + 1])
+                return false;
+
+    for (int j = 0; j < 4; j++)
+        for (int i = 0; i < 3; i++)
+            if (jadval[i][j] == jadval[i + 1][j])
+                return false;
+
+    return true;
+}
+
+bool checkwin(int jadval[4][4])
+{
+    for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 4; j++)
+            if (jadval[i][j] == 2048)
+                return true;
+    return false;
+}
+void game2048()
+{
+    srand(time(0));
+
+    cout << "Enter player name: ";
+    getline(cin, playerName);
+
+    int jadval[4][4] = {0};
+    addrandomnumber(jadval);
+    addrandomnumber(jadval);
+
+    while (true)
+    {
+        system("cls");
+        printjadval(jadval);
+        cout << "Score: " << score << endl;
+        cout << "Use keys W/A/S/D to move. Press Ctrl+C to exit.\n";
+
+        char c = _getch();
+
+        int oldjadval[4][4];
+        memcpy(oldjadval, jadval, sizeof(jadval));
+
+        if (c == 'w' || c == 'W')
+            moveupjadval(jadval);
+        else if (c == 'a' || c == 'A')
+            moveleftjadval(jadval);
+        else if (c == 'd' || c == 'D')
+            moverightjadval(jadval);
+        else if (c == 's' || c == 'S')
+            movedownjadval(jadval);
+        else
+            continue;
+
+        bool changed = false;
+        for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 4; j++)
+                if (oldjadval[i][j] != jadval[i][j])
+                    changed = true;
+
+        if (changed)
+            addrandomnumber(jadval);
+
+        if (checkwin(jadval))
+        {
+            system("cls");
+            printjadval(jadval);
+            cout << "YOU WIN\n";
+            break;
+        }
+
+        if (checklose(jadval))
+        {
+            system("cls");
+            printjadval(jadval);
+            cout << "YOU LOST\n";
+            break;
+        }
+    }
+
+    ofstream file("scores.txt", ios::app);
+    if (file.is_open())
+    {
+        file << playerName << " " << score << endl;
+        file.close();
+    }
+}
+
 int main()
 {
     startSection();
@@ -220,10 +469,8 @@ int main()
     if (game == 0)
     {
         clear();
-        setColor(15);
-        centerText("Section (2048) - later...", 12);
-        soundError();
-        Sleep(2000);
+        soundStartGame();
+        game2048();
     }
     else
     {
